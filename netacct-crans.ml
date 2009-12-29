361ba97025a80dff47f4794b19fc21138f83bb85
Stephane Glondu <steph@glondu.net>
2009-12-29 23:24:17 +0100

    Remove Affero references in license headers

--- netacct-crans.ml
+++ netacct-crans.ml
@@ -10,11 +10,10 @@
 (*  This program is distributed in the hope that it will be useful, but    *)
 (*  WITHOUT ANY WARRANTY; without even the implied warranty of             *)
 (*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *)
-(*  Affero General Public License for more details.                        *)
+(*  General Public License for more details.                               *)
 (*                                                                         *)
-(*  You should have received a copy of the GNU Affero General Public       *)
-(*  License along with this program.  If not, see                          *)
-(*  <http://www.gnu.org/licenses/>.                                        *)
+(*  You should have received a copy of the GNU General Public License      *)
+(*  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *)
 (***************************************************************************)
 
 open Printf
